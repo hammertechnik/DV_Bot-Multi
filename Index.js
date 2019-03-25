afe52ef71636da6db338030f0131d6722c467dc1
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-03-25 13:51:54 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -17,8 +17,32 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   
-  if(cmd === `${prefix}hello`){
-    return message.channel.send("Hello!:wave:");
+  if(cmd === `${prefix}serverinfo`){
+    let sEmbed = new Discord.RichEmbed()
+    let sicon = message.guild.displayAvatarURL;
+    
+    .setDescription("Server Information")
+    .setColor("#032359")
+    .addField("Server Name", bot.user.username)
+    .setThumbnail(sicon)
+    .addField("Created On", bot.user.createdAt)
+    .addField("Save Files");
+    
+    return message.channel.send(sEmbed);
+  }
+  
+  if(cmd === `${prefix}botinfo`){
+    let botEmbed = new Discord.RichEmbed()
+    let bicon = bot.user.displayAvatarURL;
+    
+    .setDescription("Bot Information")
+    .setColor("#003182")
+    .addField("Bot Name", bot.user.username)
+    .setThumbnail(bicon)
+    .addField("Created On", bot.user.createdAt)
+    .addField("Save Files");
+    
+    return message.channel.send(botEmbed);
   }
   
 });
